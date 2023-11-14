48243722568c9f464e348280365539ef5864b172
Gabriel Marcano <gabemarcano@yahoo.com>
2023-11-13 23:09:42 -0800

    Fix svl import in module

---
+++ src/svl/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+""" SparkFun Variable Loader package.
+
+Variable baud rate bootloader for Artemis Apollo3 modules, used to load
+applications to flash.
+"""
+
+from svl.svl import main
