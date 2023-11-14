af747e173b26ffc377a57841beed1e66182753bb
Gabriel Marcano <gabemarcano@yahoo.com>
2023-11-13 18:05:02 -0800

    Applied (most) pylint suggested fixes
     - Normalized variables to snake_case
     - Switched verbose handling to logging module
     - Do more pythonic conditional checking

--- src/svl/__main__.py
+++ src/svl/__main__.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+""" SparkFun Variable Loader programmer.
+
+Variable baud rate bootloader for Artemis Apollo3 modules, used to load
+applications to flash.
+
+This executes the program to flash an application.
+"""
 import svl
 
 if __name__ == "__main__":
--- src/svl/svl.py
+++ src/svl/svl.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python
-# SparkFun Variable Loader
-# Variable baud rate bootloader for Artemis Apollo3 modules
 
-# Immediately upon reset the Artemis module will search for the timing character
-#   to auto-detect the baud rate. If a valid baud rate is found the Artemis will
-#   respond with the bootloader version packet
-# If the computer receives a well-formatted version number packet at the desired
-#   baud rate it will send a command to begin bootloading. The Artemis shall then
-#   respond with the a command asking for the next frame.
-# The host will then send a frame packet. If the CRC is OK the Artemis will write
-#   that to memory and request the next frame. If the CRC fails the Artemis will
-#   discard that data and send a request to re-send the previous frame.
-# This cycle repeats until the Artemis receives a done command in place of the
-#   requested frame data command.
-# The initial baud rate determination must occur within some small timeout. Once
-#   baud rate detection has completed all additional communication will have a
-#   universal timeout value. Once the Artemis has begun requesting data it may no
-#   no longer exit the bootloader. If the host detects a timeout at any point it
-#   will stop bootloading.
+""" SparkFun Variable Loader
 
-# Notes about PySerial timeout:
-# The timeout operates on whole functions - that is to say that a call to
-#   ser.read(10) will return after ser.timeout, just as will ser.read(1) (assuming
-#   that the necessary bytes were not found)
-# If there are no incoming bytes (on the line or in the buffer) then two calls to
-#   ser.read(n) will time out after 2*ser.timeout
-# Incoming UART data is buffered behind the scenes, probably by the OS.
+Variable baud rate bootloader for Artemis Apollo3 modules.
+
+Immediately upon reset the Artemis module will search for the timing character
+to auto-detect the baud rate. If a valid baud rate is found the Artemis will
+respond with the bootloader version packet
+
+If the computer receives a well-formatted version number packet at the desired
+baud rate it will send a command to begin bootloading. The Artemis shall then
+respond with the a command asking for the next frame.
+
+The host will then send a frame packet. If the CRC is OK the Artemis will write
+that to memory and request the next frame. If the CRC fails the Artemis will
+discard that data and send a request to re-send the previous frame.
+
+This cycle repeats until the Artemis receives a done command in place of the
+requested frame data command.
+
+The initial baud rate determination must occur within some small timeout. Once
+baud rate detection has completed all additional communication will have a
+universal timeout value. Once the Artemis has begun requesting data it may no
+no longer exit the bootloader. If the host detects a timeout at any point it
+will stop bootloading.
+
+Notes about PySerial timeout:
+ - The timeout operates on whole functions - that is to say that a call to
+   ser.read(10) will return after ser.timeout, just as will ser.read(1) (assuming
+   that the necessary bytes were not found)
+ - If there are no incoming bytes (on the line or in the buffer) then two calls to
+   ser.read(n) will time out after 2*ser.timeout
+ - Incoming UART data is buffered behind the scenes, probably by the OS.
+"""
 
 # ***********************************************************************************
 #
@@ -33,14 +40,15 @@
 #
 # ***********************************************************************************
 
-import argparse
-import serial
-import serial.tools.list_ports as list_ports
 import sys
 import time
 import math
 import os.path
-from sys import exit
+import logging
+import argparse
+
+import serial
+from serial.tools import list_ports
 
 SCRIPT_VERSION_MAJOR = "1"
 SCRIPT_VERSION_MINOR = "7"
@@ -57,7 +65,7 @@ SVL_CMD_FRAME = 0x04  # indicate app data frame
 SVL_CMD_RETRY = 0x05  # request re-send frame
 SVL_CMD_DONE = 0x06  # finished - all data sent
 
-barWidthInCharacters = 50  # Width of progress bar, ie [###### % complete
+BAR_WIDTH_CHARS = 50  # Width of progress bar, ie [###### % complete
 
 crcTable = (
     0x0000,
@@ -317,31 +325,27 @@ crcTable = (
     0x8207,
     0x0202,
 )
-# ***********************************************************************************
-#
-# Compute CRC on a byte array
-#
-# ***********************************************************************************
 
 
 def get_crc16(data):
+    """Compute the CRC16 of the given data.
+
+    Keyword arguments:
+    data -- FIXME it just needs to be convertible to a bytearray?
+    """
     # Table and code ported from Artemis SVL bootloader
     crc = 0x0000
     data = bytearray(data)
     for ch in data:
-        tableAddr = ch ^ (crc >> 8)
-        CRCH = (crcTable[tableAddr] >> 8) ^ (crc & 0xFF)
-        CRCL = crcTable[tableAddr] & 0x00FF
-        crc = CRCH << 8 | CRCL
+        table_addr = ch ^ (crc >> 8)
+        crch = (crcTable[table_addr] >> 8) ^ (crc & 0xFF)
+        crcl = crcTable[table_addr] & 0x00FF
+        crc = crch << 8 | crcl
     return crc
 
 
-# ***********************************************************************************
-#
-# Wait for a packet
-#
-# ***********************************************************************************
 def wait_for_packet(ser):
+    """Wait for a packet."""
     packet = {"len": 0, "cmd": 0, "data": 0, "crc": 1, "timeout": 1}
 
     n = ser.read(2)  # get the number of bytes
@@ -366,14 +370,8 @@ def wait_for_packet(ser):
     return packet
 
 
-# ***********************************************************************************
-#
-# Send a packet
-#
-# ***********************************************************************************
-
-
 def send_packet(ser, cmd, data):
+    """Send a packet."""
     data = bytearray(data)
     num_bytes = 3 + len(data)
     payload = bytearray(cmd.to_bytes(1, "big"))
@@ -385,19 +383,15 @@ def send_packet(ser, cmd, data):
     ser.write(bytes(payload))
 
 
-# ***********************************************************************************
-#
-# Setup: signal baud rate, get version, and command BL enter
-#
-# ***********************************************************************************
 def phase_setup(ser):
+    """Setup: signal baud rate, get version, and command BL enter."""
     baud_detect_byte = b"U"
 
-    verboseprint("\nPhase:\tSetup")
+    logging.debug("Phase:\tSetup")
 
     # Handle the serial startup blip
     ser.reset_input_buffer()
-    verboseprint("\tCleared startup blip")
+    logging.debug("\tCleared startup blip")
 
     ser.write(baud_detect_byte)  # send the baud detection character
 
@@ -405,11 +399,10 @@ def phase_setup(ser):
     if packet["timeout"] or packet["crc"]:
         return False  # failed to enter bootloader
 
-    twopartprint(
-        "\t",
+    print(
         "Got SVL Bootloader Version: " + str(int.from_bytes(packet["data"], "big")),
     )
-    verboseprint("\tSending 'enter bootloader' command")
+    logging.debug("\tSending 'enter bootloader' command")
 
     send_packet(ser, SVL_CMD_BL, b"")
 
@@ -418,19 +411,15 @@ def phase_setup(ser):
     # Now enter the bootload phase
 
 
-# ***********************************************************************************
-#
-# Bootloader phase (Artemis is locked in)
-#
-# ***********************************************************************************
 def phase_bootload(ser, binpath, verbose):
-    startTime = time.time()
+    """Bootloader phase (Artemis is locked in)."""
+    start_time = time.time()
     frame_size = 512 * 4
 
     resend_max = 4
     resend_count = 0
 
-    verboseprint("\nPhase:\tBootload")
+    logging.debug("\nPhase:\tBootload")
 
     with open(binpath, mode="rb") as binfile:
         application = binfile.read()
@@ -438,37 +427,29 @@ def phase_bootload(ser, binpath, verbose):
 
         total_frames = math.ceil(total_len / frame_size)
         curr_frame = 0
-        progressChars = 0
+        progress_chars = 0
 
         if not verbose:
             print("[", end="")
 
-        verboseprint(
-            "\thave "
-            + str(total_len)
-            + " bytes to send in "
-            + str(total_frames)
-            + " frames"
-        )
+        logging.debug("\thave %s bytes to send in %s frames", total_len, total_frames)
 
         bl_done = False
         bl_succeeded = True
-        while (bl_done == False) and (bl_succeeded == True):
+        while (not bl_done) and bl_succeeded:
             # wait for indication by Artemis
             packet = wait_for_packet(ser)
             if packet["timeout"] or packet["crc"]:
-                verboseprint("\n\tError receiving packet")
-                verboseprint(packet)
-                verboseprint("\n")
+                logging.debug("\tError receiving packet")
+                logging.debug(packet)
                 bl_succeeded = False
                 bl_done = True
 
             if packet["cmd"] == SVL_CMD_NEXT:
-                # verboseprint('\tgot frame request')
                 curr_frame += 1
                 resend_count = 0
             elif packet["cmd"] == SVL_CMD_RETRY:
-                verboseprint("\t\tRetrying...")
+                logging.debug("\t\tRetrying...")
                 resend_count += 1
                 if resend_count >= resend_max:
                     bl_succeeded = False
@@ -485,21 +466,18 @@ def phase_bootload(ser, binpath, verbose):
                     )
                 ]
                 if verbose:
-                    verboseprint(
-                        "\tSending frame #"
-                        + str(curr_frame)
-                        + ", length: "
-                        + str(len(frame_data))
+                    logging.debug(
+                        "\tSending frame #%s, length: %s", curr_frame, len(frame_data)
                     )
                 else:
-                    percentComplete = curr_frame * 100 / total_frames
-                    percentCompleteInChars = math.ceil(
-                        percentComplete / 100 * barWidthInCharacters
+                    percent_complete = curr_frame * 100 / total_frames
+                    percent_complete_in_chars = math.ceil(
+                        percent_complete / 100 * BAR_WIDTH_CHARS
                     )
-                    while progressChars < percentCompleteInChars:
-                        progressChars = progressChars + 1
+                    while progress_chars < percent_complete_in_chars:
+                        progress_chars = progress_chars + 1
                         print("#", end="", flush=True)
-                    if percentComplete == 100:
+                    if percent_complete == 100:
                         print("]", end="")
 
                 send_packet(ser, SVL_CMD_FRAME, frame_data)
@@ -508,23 +486,19 @@ def phase_bootload(ser, binpath, verbose):
                 send_packet(ser, SVL_CMD_DONE, b"")
                 bl_done = True
 
-        if bl_succeeded == True:
-            twopartprint("\n\t", "Upload complete")
-            endTime = time.time()
-            bps = total_len / (endTime - startTime)
-            verboseprint("\n\tNominal bootload bps: " + str(round(bps, 2)))
+        if bl_succeeded:
+            print("Upload complete")
+            end_time = time.time()
+            bps = total_len / (end_time - start_time)
+            logging.debug("\n\tNominal bootload bps: %s", round(bps, 2))
         else:
-            twopartprint("\n\t", "Upload failed")
+            print("Upload failed")
 
         return bl_succeeded
 
 
-# ***********************************************************************************
-#
-# Help if serial port could not be opened
-#
-# ***********************************************************************************
 def phase_serial_port_help(port):
+    """Help if serial port could not be opened."""
     devices = list_ports.comports()
 
     # First check to see if user has the given port open
@@ -536,7 +510,7 @@ def phase_serial_port_help(port):
                 + dev.device
                 + " and try again."
             )
-            exit()
+            sys.exit()
 
     # otherwise, give user a list of possible com ports
     print(port.upper() + " not found but we detected the following serial ports:")
@@ -561,12 +535,8 @@ def phase_serial_port_help(port):
             print(dev.description)
 
 
-# ***********************************************************************************
-#
-# Main function
-#
-# ***********************************************************************************
 def main():
+    """Main function."""
     parser = argparse.ArgumentParser(
         description="SparkFun Serial Bootloader for Artemis"
     )
@@ -608,44 +578,29 @@ def main():
 
     args = parser.parse_args()
 
-    # Create print function for verbose output if caller deems it: https://stackoverflow.com/questions/5980042/how-to-implement-the-verbose-or-v-option-into-a-script
     if args.verbose:
-
-        def verboseprint(*args):
-            # Print each argument separately so caller doesn't need to
-            # stuff everything to be printed into a single string
-            for arg in args:
-                print(arg, end="", flush=True),
-            print()
-
-    else:
-        verboseprint = lambda *a: None  # do-nothing function
-
-    def twopartprint(verbosestr, printstr):
-        if args.verbose:
-            print(verbosestr, end="")
-
-        print(printstr)
+        logging.getLogger().setLevel(logging.DEBUG)
 
     try:
         num_tries = 3
 
         print("\n\nArtemis SVL Bootloader")
 
-        verboseprint(
-            "Script version " + SCRIPT_VERSION_MAJOR + "." + SCRIPT_VERSION_MINOR
+        logging.debug(
+            "Script version %s.%s", SCRIPT_VERSION_MAJOR, SCRIPT_VERSION_MINOR
         )
 
         if not os.path.exists(args.binfile):
-            print("Bin file {} does not exist.".format(args.binfile))
-            exit()
+            print(f"Bin file {args.binfile} does not exist.")
+            sys.exit()
 
         bl_success = False
         entered_bootloader = False
 
         for _ in range(num_tries):
             with serial.Serial(args.port, args.baud, timeout=args.timeout) as ser:
-                # startup time for Artemis bootloader   (experimentally determined - 0.095 sec min delay)
+                # startup time for Artemis bootloader
+                # (experimentally determined - 0.095 sec min delay)
                 t_su = 0.15
 
                 time.sleep(t_su)  # Allow Artemis to come out of reset
@@ -653,26 +608,27 @@ def main():
                 # Perform baud rate negotiation
                 entered_bootloader = phase_setup(ser)
 
-                if entered_bootloader == True:
+                if entered_bootloader:
                     bl_success = phase_bootload(ser, args.binfile, args.verbose)
-                    if bl_success == True:  # Bootload
+                    if bl_success:  # Bootload
                         # print("Bootload complete!")
                         break
                 else:
-                    verboseprint("Failed to enter bootload phase")
+                    logging.debug("Failed to enter bootload phase")
 
-            if bl_success == True:
+            if bl_success:
                 break
 
-        if entered_bootloader == False:
+        if not entered_bootloader:
             print(
-                "Target failed to enter bootload mode. Verify the right COM port is selected and that your board has the SVL bootloader."
+                "Target failed to enter bootload mode.",
+                "Verify the right COM port is selected and that your board has the SVL bootloader.",
             )
 
     except serial.SerialException:
         phase_serial_port_help(args.port)
 
-    exit()
+    sys.exit()
 
 
 # ******************************************************************************
